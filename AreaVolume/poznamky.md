8a2605ec52ea4cde8f9663cf52f142d90ab0bb49
Ondřej Novotný <onovotny08@gmail.com>
2026-09-17 11:27:39 +0200

    faktorial vypocet program

---
+++ AreaVolume/poznamky.md
@@ -0,0 +1,5 @@
+dotnet new sln - nove solution
+
+dotnet new console -n AreaVolume - vytvoreni nove konzolove aplikace
+
+dotnet sln add AreaVolume/AreaVolume.csproj nalinkovani projektu do solution
---
+++ Factorial1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadejte celé nezáporné číslo: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            if (n < 0)
+            {
+                Console.WriteLine("Faktoriál záporného čísla není definován.");
+            }
+            else
+            {
+                int vysledek = VypocitejFaktorial(n);
+                Console.WriteLine($"{n}! = {vysledek}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Zadaná hodnota není platné celé číslo.");
+        }
+    }
+ 
+    static int VypocitejFaktorial(int n)
+    {
+
+        if (n <= 1)
+        {
+            return 1;
+        }
+        return n * VypocitejFaktorial(n - 1);
+    }
+}
